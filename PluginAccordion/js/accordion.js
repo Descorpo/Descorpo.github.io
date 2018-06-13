402dfb7e75f0e37e6721b4f90cf3bfe4e899fa6a
Descorpo <stafua007@gmail.com>
2018-06-13 15:01:45 +0300

    Accordion

---
+++ PluginAccordion/js/accordion.js
@@ -0,0 +1,33 @@
+const accordionBtn = $('[data-toggle-accordion]');
+
+function onclick(e) {
+    const header = $(this).children('.accordion-header');
+
+    const item = $(this).children('.accordion-body');
+
+    if(item.is(':visible')) {
+        item.slideUp(300);
+
+        header.css({
+            'background': '#f7f7f7',
+            'color': '#000'
+        });
+    } else {
+        accordionBtn.find('>.accordion-header').css({
+            'background': '#f7f7f7',
+            'color': '#000'
+        });
+
+        accordionBtn.find('>.accordion-body').slideUp(300);
+
+        item.slideDown(300);
+
+        header.css({
+            'background': '#007bff',
+            'color': '#fff'
+        });
+    }
+}
+
+accordionBtn.on('click', onclick);
+
